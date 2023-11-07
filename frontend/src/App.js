584d6abee8cdfc75d85710776cd4219ad612d2e5
Heeeeeppo <mqfang211@gmail.com>
2023-11-07 18:48:17 -0500

    modified App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,24 @@ function App() {
     const [todos, setTodos] = useState([]);
     const [newTodo, setNewTodo] = useState("");
 
+    function handleChange() {
+        
+    }
+
     return (
         <div className="App">
             <h4>Your Tasks</h4>
+            <div>
+                <label for="text">Todo</label>
+                <input 
+                    type="text" 
+                    name="text" 
+                    onChange={handleChange} 
+                    value={newTodo} 
+                    placeholder="Create a new todo"
+                />
+            </div>
+            
             <div className="todos">
                 <div className="todo">
                     <div className="checkbox"></div>
